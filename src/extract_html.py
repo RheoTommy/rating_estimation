203f19669d93efafe3427fc38335720af07028f5
RheoTommy <tommyrheo@gmail.com>
2021-12-31 16:10:28 +0900

    sort_csv 追加

--- src/extract_html.py
+++ src/extract_html.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import requests
 
 
 def source_code_extractor(html: str) -> str:
@@ -8,11 +9,19 @@ def source_code_extractor(html: str) -> str:
     return dom[0].text
 
 
+def create_submission_url(contest: str, submission_id: str) -> str:
+    return "https://atcoder.jp/contests/{}/submissions/{}".format(contest, submission_id)
+
+
 def test_source_code_extractor():
-    with open("data/26320598.html") as f:
-        s = f.read()
+    url = create_submission_url("abc213", "24866073")
+    r = requests.get(url)
+    if r.ok:
+        s = r.text
         s = source_code_extractor(s)
         print(s)
+    else:
+        print("bad request: {}".format(r.reason))
 
 
 test_source_code_extractor()
