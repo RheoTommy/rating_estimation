13783ac883ab0ef343ed5d22c452770fa283ea92
RheoTommy <tommyrheo@gmail.com>
2021-12-31 12:27:14 +0900

    さようなら ジュピター

--- src/data_downloader.py
+++ src/data_downloader.py
@@ -1,9 +1,19 @@
-import csv
+import pandas as pd
+import tqdm
+
 
 def sample():
-    with open("submissions.csv") as f:
-        reader = csv.reader(f)
-        l = [row for row in reader]
-        l.sort()
-        for i in range(100):
-            print(l[i])
+    file_name = "submissions.csv"
+    reader = pd.read_csv(file_name, chunksize=1000000)
+    df = pd.DataFrame([])
+    for r in tqdm.tqdm(reader):
+        df = pd.concat([df, r.query("1609588800<=epoch_second")])
+        # df = pd.concat([df, r.query("epoch_second>=1609588800").filter(like="abc")])
+        # df = pd.concat([df, r.query("epoch_second>=1609588800").filter(like="arc")])
+        # df = pd.concat([df, r.query("epoch_second>=1609588800").filter(like="agc")])
+
+    print(df.head())
+    print(df.shape)
+
+
+sample()
