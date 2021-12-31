95d144ded23680faa8f8fcb9b3e143c5f5cd55bd
RheoTommy <tommyrheo@gmail.com>
2021-12-31 23:22:38 +0900

    rating at the time を取得する関数の作成 テストは途中まで

--- src/sample.py
+++
@@ -1,8 +0,0 @@
-import time
-from datetime import datetime
-
-unix_time = datetime.fromisoformat("2021-11-27T22:40:00+09:00")  # abc229
-print(unix_time)
-print(unix_time.timestamp())
-
-print(datetime.now().timestamp())
