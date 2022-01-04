fea7bf5fe620ebf81b215833fa57caf0a93df9b7
yuto1115 <yutown1115@gmail.com>
2022-01-04 17:00:36 +0900

    処理を別ファイルに分けた

---
+++ src/get_user_histories.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import tqdm
+import time
+
+from lib.user import get_user_history, save_all_user_histories
+
+
+def get_all_user_histories():
+    file_name = "csv/extract.csv"
+    df = pd.read_csv(file_name)
+    user_id_list = df["user_id"].unique()
+    user_histories = {}
+    for user_id in tqdm.tqdm(user_id_list):
+        while True:
+            try:
+                time.sleep(0.5)
+                user_histories[user_id] = get_user_history(user_id)
+            except Exception as e:
+                print(e)
+            else:
+                break
+    save_all_user_histories(user_histories)
+
+
+get_all_user_histories()
