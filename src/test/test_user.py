8f070ef420ddafddb13b634340d14f4de6daa565
RheoTommy <tommyrheo@gmail.com>
2022-01-04 17:59:44 +0900

    テストの変更

--- src/test/test_user.py
+++ src/test/test_user.py
@@ -13,4 +13,4 @@ def test_get_rating():
     epoch_seconds = [1610194106, 1616324772, 1621767984, 1640520181]
     expect = [2277, 2488, 2536, 2594]
     for i in range(4):
-        assert get_rating(user_id, epoch_seconds[i]) == expect[i]
+        assert get_rating([(user_id, epoch_seconds[i])])[0] == expect[i]
