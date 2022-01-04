0e811a9f5249d3b3f7d399e0ce74f82b96b235bf
RheoTommy <tommyrheo@gmail.com>
2022-01-04 17:38:24 +0900

    extract_csv で上限を設定 get_user_histories で手元も結果を再利用するようにした

--- src/data_preparation/extract_csv.py
+++ src/data_preparation/extract_csv.py
@@ -18,7 +18,7 @@ def extract_csv():
     reader = pd.read_csv(file_name, chunksize=1000000)
     df = pd.DataFrame([])
     for r in tqdm.tqdm(reader):
-        dfi = r.query("1609588800<=epoch_second").query("language.str.startswith('C++')", engine="python")
+        dfi = r.query("1609588800<=epoch_second<1640962800").query("language.str.startswith('C++')", engine="python")
         df = pd.concat([df, dfi.query("contest_id.apply(@filter_with_contest_id)", engine="python")])
 
     print(df.head())
--- src/data_preparation/get_user_histories.py
+++ src/data_preparation/get_user_histories.py
@@ -2,16 +2,16 @@ import pandas as pd
 import tqdm
 import time
 
-from src.lib.user import get_user_history, save_all_user_histories
+from src.lib.user import get_user_history, save_all_user_histories, load_all_user_histories
 
 
 def get_all_user_histories():
     file_name = "csv/extract.csv"
     df = pd.read_csv(file_name)
     user_id_list = df["user_id"].unique()
-    user_histories = {}
+    user_histories = load_all_user_histories()
     for user_id in tqdm.tqdm(user_id_list):
-        while True:
+        while user_id in user_histories:
             try:
                 time.sleep(0.5)
                 user_histories[user_id] = get_user_history(user_id)
