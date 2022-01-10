fb9a5298fb36a05336c685b7e71e9dc34762908e
RheoTommy <tommyrheo@gmail.com>
2022-01-10 17:53:20 +0900

    ソースコードを取得し，保存する関数の作成

---
+++ src/data_preparation/get_source_codes.py
@@ -0,0 +1,21 @@
+import time
+
+from tqdm import tqdm
+
+from src.lib.extract_html import get_source_code
+from src.lib.submissions import load_all_submissions
+
+
+def get_all_source_codes():
+    submissions = load_all_submissions()
+    for submission in tqdm(submissions):
+        for _ in range(5):
+            try:
+                time.sleep(0.1)
+                source_code = get_source_code(submission.contest_id, submission.submission_id)
+                with open("source_codes/{}.cpp".format(submission.submission_id), "wb") as f:
+                    f.write(source_code.encode())
+            except Exception as e:
+                print(e)
+            else:
+                break
