d94e881664e565e48951525b93b570f9dd57adcd
yuto1115 <yutown1115@gmail.com>
2022-01-11 00:19:32 +0900

    main 関数の中身だけ抜き出す

---
+++ src/lib/source_code_handling.py
@@ -0,0 +1,25 @@
+import re
+
+
+def extract_str_in_main(code: str) -> str:
+    cnt_main = len(re.findall(r" main *\( *\) *{", code))
+    if cnt_main == 0:
+        raise Exception("なんで main 関数がないんや")
+    elif cnt_main > 1:
+        raise Exception("main 関数が2つ以上あるだなんて，そんなアホな")
+    start = re.search(r" main *\( *\) *{", code).end()
+    end = start
+    now = 1
+    while True:
+        if end == len(code):
+            raise Exception("main 関数の終わりが見つからねえ！")
+        ch = code[end]
+        print(ch)
+        if ch == '{':
+            now += 1
+        elif ch == '}':
+            now -= 1
+        if now == 0:
+            break
+        end += 1
+    return code[start:end]
