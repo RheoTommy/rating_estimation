ac2f8b4d428993020d8fb2e8fdc5117da664250a
yuto1115 <yutown1115@gmail.com>
2022-01-11 00:33:10 +0900

    関数をいっぱい作ってみたよ

--- src/single_characteristics/analyze_characteristics.py
+++ src/single_characteristics/analyze_characteristics.py
@@ -1,12 +1,14 @@
-from src.lib.submissions import Submission, filtered_submissions, load_all_submissions, load_all_available_submissions, \
-    with_source_codes
-from src.lib.data_handling import standardize
-from typing import Callable, List, Tuple
+from typing import Callable
+
 import numpy as np
+
+from src.lib.data_handling import standardize
+from src.lib.submissions import filtered_submissions, load_all_submissions, with_source_codes
+from word_count import *
 
 
 def sampling() -> List[Tuple[Submission, str]]:
-    sample_size = 1
+    sample_size = 500
     # 提出データのローカル保存が終わるまで
     all_sub = filtered_submissions(load_all_submissions())
     idx = np.random.choice(np.arange(len(all_sub)), sample_size, replace=False)
@@ -41,4 +43,17 @@ def testing(func: Callable[[List[Tuple[Submission, str]]], List[float]], dataset
 
 
 dataset = sampling()
-# testing(, dataset)
+testing(code_length, dataset)
+testing(word_count_define, dataset)
+testing(word_count_using, dataset)
+testing(word_count_define_int_long_long, dataset)
+testing(word_count_for, dataset)
+testing(word_count_in_main_for, dataset)
+testing(word_count_if, dataset)
+testing(word_count_in_main_if, dataset)
+testing(word_count_vector, dataset)
+testing(word_count_in_main_vector, dataset)
+testing(word_count_rep, dataset)
+testing(word_count_in_main_rep, dataset)
+testing(word_count_auto, dataset)
+testing(word_count_in_main_auto, dataset)
