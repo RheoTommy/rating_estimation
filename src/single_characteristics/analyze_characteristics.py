b4fc8ab8ec987fd510d1904e89f870a64bf811db
yuto1115 <yutown1115@gmail.com>
2022-01-10 23:10:47 +0900

    available_submissions の導入に伴う sampling の変更

--- src/single_characteristics/analyze_characteristics.py
+++ src/single_characteristics/analyze_characteristics.py
@@ -1,4 +1,5 @@
-from src.lib.submissions import Submission, filtered_submissions, load_all_submissions, with_source_codes
+from src.lib.submissions import Submission, filtered_submissions, load_all_submissions, load_all_available_submissions, \
+    with_source_codes
 from src.lib.data_handling import standardize
 from typing import Callable, List, Tuple
 import numpy as np
@@ -6,6 +7,7 @@ import numpy as np
 
 def sampling() -> List[Tuple[Submission, str]]:
     sample_size = 1
+    # 提出データのローカル保存が終わるまで
     all_sub = filtered_submissions(load_all_submissions())
     idx = np.random.choice(np.arange(len(all_sub)), sample_size, replace=False)
     sample_sub = []
@@ -14,13 +16,19 @@ def sampling() -> List[Tuple[Submission, str]]:
     print("start getting source codes")
     res = with_source_codes(sample_sub)
     print("finish getting source codes")
-    # 提出データのローカル保存が終われば，このまま return res すれば良い
-    res2 = []
-    for sub, s in res:
-        if s != "":
-            res2.append((sub, s))
-    print("available data count : {}".format(len(res2)))
-    return res2
+    res = list(filter(lambda x: x[1] != "", res))
+    print("available data count : {}".format(len(res)))
+    return res
+    # 提出データのローカル保存が終わったら
+    # all_sub = load_all_available_submissions()
+    # idx = np.random.choice(np.arange(len(all_sub)), sample_size, replace=False)
+    # sample_sub = []
+    # for i in idx:
+    #     sample_sub.append(all_sub[i])
+    # print("start getting source codes")
+    # res = with_source_codes(sample_sub)
+    # print("finish getting source codes")
+    # return res
 
 
 # func : (dataset: List[Submission]) -> (features: List[float])
