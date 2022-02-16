b7a864c25dbd88367dab30273368be909cf41e39
yuto1115 <yutown1115@gmail.com>
2022-02-16 17:15:19 +0900

    コンパイル関係

---
+++ src/data_preparation/compile.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+from typing import List
+from joblib import Parallel, delayed
+from tqdm import tqdm
+
+from src.lib.submissions import Submission, load_all_available_submissions
+
+
+def prepare_assemblers(submissions: List[Submission]):
+    def f(s: Submission):
+        if not os.path.isfile("assembler/{}.s".format(s.submission_id)):
+            command = "docker exec atc g++ -std=gnu++17 -w -O2 -DONLINE_JUDGE -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I/opt/ac-library -S /source_codes/{}.cpp".format(
+                s.submission_id)
+            res = subprocess.check_call(command, shell=True)
+            assert res == 0
+            res = subprocess.check_call(["docker", "cp", "atc:/{}.s".format(s.submission_id), "./assembler"])
+            assert res == 0
+
+    Parallel(n_jobs=-1)(delayed(f)(s) for s in tqdm(submissions))
+
+
+def prepare_pps(submissions: List[Submission]):
+    def f(s: Submission):
+        if not os.path.isfile("pp/{}.cpp".format(s.submission_id)):
+            res = subprocess.check_call("docker exec atc touch {}.cpp".format(s.submission_id), shell=True)
+            assert res == 0
+            command = "docker exec atc g++ -std=gnu++17 -w -O2 -DONLINE_JUDGE -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I/opt/ac-library -E -P /source_codes/{}.cpp > {}.cpp".format(
+                s.submission_id, s.submission_id)
+            res = subprocess.check_call(command, shell=True)
+            assert res == 0
+            res = subprocess.check_call(["docker", "cp", "atc:/{}.cpp".format(s.submission_id), "./pp"])
+            assert res == 0
+
+    Parallel(n_jobs=-1)(delayed(f)(s) for s in tqdm(submissions))
+
+
+sub = load_all_available_submissions()
+# prepare_assemblers(sub)
+prepare_pps(sub)
