1556de31e5ac238493dfeba42123188748b459b9
RheoTommy <tommyrheo@gmail.com>
2022-02-17 12:08:38 +0900

    g++ -> g++-9

--- src/data_preparation/compile.py
+++ src/data_preparation/compile.py
@@ -22,7 +22,7 @@ def stop_docker():
 def prepare_assemblers(submissions: List[Submission]):
     def f(s: Submission):
         if not os.path.isfile("assembler/{}.s".format(s.submission_id)):
-            command = "docker exec cpp_compiler g++ -std=gnu++17 -w -O2 -DONLINE_JUDGE -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I/opt/ac-library -S /source_codes/{}.cpp".format(
+            command = "docker exec cpp_compiler g++-9 -std=gnu++17 -w -O2 -DONLINE_JUDGE -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I/opt/ac-library -S /source_codes/{}.cpp".format(
                 s.submission_id)
             res = subprocess.check_call(command, shell=True)
             assert res == 0
@@ -40,7 +40,7 @@ def prepare_pps(submissions: List[Submission]):
         if not os.path.isfile("pp/{}.cpp".format(s.submission_id)):
             res = subprocess.check_call("docker exec cpp_compiler touch {}.cpp".format(s.submission_id), shell=True)
             assert res == 0
-            command = "docker exec cpp_compiler g++ -std=gnu++17 -w -O2 -DONLINE_JUDGE -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I/opt/ac-library -E -P /source_codes/{}.cpp > /{}.cpp".format(
+            command = "docker exec cpp_compiler g++-9 -std=gnu++17 -w -O2 -DONLINE_JUDGE -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I/opt/ac-library -E -P /source_codes/{}.cpp > /{}.cpp".format(
                 s.submission_id, s.submission_id)
             res = subprocess.check_call(command, shell=True)
             assert res == 0
